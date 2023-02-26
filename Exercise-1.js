6912d766f3fdc6da9a3ade56a3d823bb47ef39e9
unknown <david.stoimenov.5@gmail.com>
2023-02-26 19:32:02 +0100

    .

---
+++ Exercise-1.js
@@ -0,0 +1,22 @@
+//Exercise 1
+
+let numbers = [1, 2, 3, 4, 5, 6];
+let nums = [];
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
+
+let avg = sum / numbers.length;
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > avg) {
+        nums.push(numbers[i]);
+    }
+}
+
+console.log(nums)
+
+//Exercise 1
+
---
+++ Exercise-2.js
@@ -0,0 +1,14 @@
+//Exercise 2
+
+let strings = ["Jocka", "Test", "Test1", "Test", "Marko", "Marko"];
+
+for (let i=0; i<strings.length; i++){
+    for (let j=0; j<strings.length; j++){
+        if(strings[i]===strings[j]){
+            
+        }
+    }
+}
+
+
+//Exercise 2
